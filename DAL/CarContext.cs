82dfb6453e6fb0f8c93a911c706359eb7b1e5351
Simon Gábor <simon.gabor@autsoft.hu>
2022-04-06 16:58:26 +0200

    Add project files.

---
+++ DAL/CarContext.cs
@@ -0,0 +1,14 @@
+﻿using Library1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class CarContext : DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options)
+          : base(options)
+        {
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
