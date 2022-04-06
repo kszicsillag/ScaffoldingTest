e9259d6d342e3206cdffb745b6890ac8b3559cda
Simon Gábor <simon.gabor@autsoft.hu>
2022-04-06 17:05:11 +0200

    merge projects

---
+++ ScaffoldingTestWeb/Car.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library1.Models
+{
+    public class Car
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int ManufacturerID { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+    }
+    public class Manufacturer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
